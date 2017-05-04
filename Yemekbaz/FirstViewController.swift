1ae2e4e7ed59d0fda3a91012ccb7be06107a1841
Sylvester Saracevas <saracevass@gmail.com>
2017-05-04 22:17:47 +0100

    Initial Commit

---
+++ Yemekbaz/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Yemekbaz
+//
+//  Created by Sylvester Saracevas on 04/05/2017.
+//  Copyright © 2017 Sylvester Saracevas. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Yemekbaz/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Yemekbaz
+//
+//  Created by Sylvester Saracevas on 04/05/2017.
+//  Copyright © 2017 Sylvester Saracevas. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
